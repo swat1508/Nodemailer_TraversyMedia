c496fc23d1ebf74474063663d0ca07753b2d8006
Swatantra Sinha <swasinha1@publicisgroupe.net>
2019-04-09 21:52:00 +0530

    first commit

--- app.js
+++ app.js
@@ -35,14 +35,19 @@ app.post('/send', (req, res) => {
     <p>${req.body.message}</p>
   `;
 
+  main();
+/*
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'mail.YOURDOMAIN.com',
-    port: 587,
+  //  host: 'mail.YOURDOMAIN.com',
+  //host: 'smtp.gmail.com',
+  //service: 'gmail',
+    host: 'mail.google.com',
+    port: 2525,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'YOUREMAIL', // generated ethereal user
-        pass: 'YOURPASSWORD'  // generated ethereal password
+        user: 'swarajsinha150889@gmail.com', // generated ethereal user
+        pass: 'MID@ugu$t1988'  // generated ethereal password
     },
     tls:{
       rejectUnauthorized:false
@@ -51,8 +56,8 @@ app.post('/send', (req, res) => {
 
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Nodemailer Contact" <your@email.com>', // sender address
-      to: 'RECEIVEREMAILS', // list of receivers
+      from: '"Swaraj Sinha" <swarajsinha150889@gmail.com>', // sender address
+      to: 'swatantrasinha1508@gmail.com', // list of receivers
       subject: 'Node Contact Request', // Subject line
       text: 'Hello world?', // plain text body
       html: output // html body
@@ -61,6 +66,7 @@ app.post('/send', (req, res) => {
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
+        console.log('some error occured !!!');
           return console.log(error);
       }
       console.log('Message sent: %s', info.messageId);   
@@ -68,6 +74,46 @@ app.post('/send', (req, res) => {
 
       res.render('contact', {msg:'Email has been sent'});
   });
+  */
   });
+
+
+//new changes starts
+async function main(){
+console.log('in main method !!!');
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "mail.google.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'swarajsinha150889@gmail.com', // generated ethereal user
+      pass: 'MID@ugu$t1988'  // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Swaraj Sinha" <swarajsinha150889@gmail.com>', // sender address
+    to: 'swatantrasinha1508@gmail.com', // list of receivers
+    subject: "Hello World !!!", // Subject line
+    text: "Hello world yo yo yoy ", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
+//new changes ends
 
 app.listen(3000, () => console.log('Server started...'));
